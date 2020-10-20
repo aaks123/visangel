2e01a3fd844bf85240327f63077a49f7a91054f5
divyanshi-ojha <72155909+divyanshi-ojha@users.noreply.github.com>
2020-10-20 10:15:19 +0100

    port->PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var app = express();
 var routes = require("./routes");
-var port = process.env.port || 8080
+var port = process.env.PORT || 8080
 
 app.use(express.static(__dirname+'/public'));
 
