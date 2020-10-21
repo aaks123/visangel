3374ffa937a5bf3ed86c1c11986f674c2ad10ac2
Divyanshi Ojha <30977696+divyanshi-ojha@users.noreply.github.com>
2020-10-21 16:49:46 +0530

    added date

--- routes.js
+++ routes.js
@@ -13,9 +13,11 @@ router.post('/addDonation',function(req,res){
     var user = req.body.name;
     var amount = req.body.amount;
     var org = req.body.org;
+    var date = req.body.date;
     var reply = `A donation of amount ${amount} has been made by ${user} to ${org}`;
     console.log(reply);
     res.send(reply);
+
 })
 
 module.exports = router;
