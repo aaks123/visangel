90c389b28db339f6fa9e7430f2834b46d2b0ced3
Hari Hara Prasath K S <hari32147@gmail.com>
2020-10-21 13:52:31 +0530

    Merges Sidharth's Fixes

--- public/extension/contentScript.js
+++ public/extension/contentScript.js
@@ -1,15 +1,16 @@
 chrome.runtime.onMessage.addListener(
 function (message,sender,sendresponse)
 {
-	console.log("Hellp");
 	var amount = document.getElementById("sc-subtotal-amount-buybox").textContent;
+	console.log(amount);
+	amount = amount.replace(",", "");
 	var name = document.getElementsByClassName("nav-line-1-container")[0].textContent;
-	console.log(name);
 	amount = parseInt(amount);
 	var model = {
 		name: name,
 		amount: amount
 	};
+	console.log(name);
 	console.log(amount);
 	sendresponse(model);
 });
